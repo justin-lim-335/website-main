74df5e2d759170cc2e258a377056b7ea4ca79580
justin-lim-335 <justinlim0223@gmail.com>
2025-11-01 03:12:23 -0700

    Last tweaks for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // important for Vercel
+  base: '/', // absolute path for Vercel
   build: {
     outDir: 'dist',
   },
   server: {
     port: 5173,
+    cors: true,
   },
 })
