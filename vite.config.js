798ed6180d05eb6b58c807d9181276f8ca99cbb6
justin-lim-335 <justinlim0223@gmail.com>
2025-11-01 03:00:13 -0700

    improved configuration

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
-export default {
-  base: '/',
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // important for Vercel
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+  },
+})
